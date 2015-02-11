d441fdd263bd2887fb6de994770e77b1ada08119
Adithya Pothuri <aditp91@gmail.com>
2015-02-11 09:52:55 -0500

    TVRage Data Source implementation started

---
+++ server/methods/TvRageInformationProvider.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Adi on 2/11/15.
+ */
+
+var BASE_URL = "http://services.tvrage.com/feeds/";
+
+var getShows = function (search_show) {
+
+    var shows = search_show;
+
+    return shows;
+
+};
+
+var getFullDetails = function (search_showId) {
+
+};
