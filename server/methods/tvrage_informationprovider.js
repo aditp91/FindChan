54f9370877b818eb25c303664bdbf2139cdc3f93
Adithya Pothuri <aditp91@gmail.com>
2015-02-17 18:23:10 -0500

    http get request to TVRage

--- server/methods/tvrage_informationprovider.js
+++ server/methods/tvrage_informationprovider.js
@@ -6,21 +6,29 @@ Meteor.methods({
     getShows: function (showquery) {
 
         //change
-        showquery = "arrow";
+        showquery = "walking";
 
         var BASE_URL = "http://services.tvrage.com/feeds/";
         var url = BASE_URL + 'search.php?show=' + showquery;
         var shows = "";
+        var result = "";
         this.unblock();
 
         // Make HTTP get request and display code and XML result
         try {
-            shows = HTTP.call('GET', url);
-            console.log(shows.statusCode);
+            result = HTTP.call('GET', url);
+            console.log(result.statusCode);
         } catch (e) {
             console.log('Exception calling', url);
             throw e;
         }
+
+        // parse the xml
+        var xmlDoc = $.parseXML(result.content);
+
+
+
+
 
         return shows;
     }
