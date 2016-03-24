efb1cf571a92d9b8c9e66b3d7a954f14b998422c
Adithya Pothuri <aditp91@gmail.com>
2016-03-24 14:32:40 -0400

    information changes

--- server/methods/tvrage_informationprovider.js
+++ server/methods/tvrage_informationprovider.js
@@ -24,19 +24,22 @@ Meteor.methods({
         }
 
         var array = JSON.parse(result.content);
-        parseShows(array);
+        //parseShows(array);
     }
 });
 
 function parseShows(showsArray) {
     // Make Show objects from JSON objects
     var showObjects = [];
-    for (i=0; i < 5; i++) {
-        console.log("Show obj is: " + JSON.stringify(showsArray[i].show));
-        if (showsArray[i].show.status === "Running") {
+    for (var i=0; i < 200; i++) {
+        //console.log("Show obj is: " + JSON.stringify(showsArray[i].show));
+        if ((showsArray[i] != null) && showsArray[i].show.status === "Running") {
             var show_name = showsArray[i].show.name;
             var show_id = showsArray[i].show.id;
-            var showobj = new Show(show_name, show_id);
+            var show_channel_name = showsArray[i].show.network.name;
+            var show_channel_number = showsArray[i].show.network.id;
+
+            var showobj = new Show(show_name, show_id, show_channel_number, show_channel_name);
 
             showobj.printDetails();
             showObjects.push(showobj);
@@ -48,21 +51,25 @@ function parseShows(showsArray) {
 // NEXT STEP IS TO PERSIST OBJECT TO DATABASE (if it doesn't exist already)
 // TVLISTINGS TABLE + MAKE NEW TABLE FOR JUST SHOWS
 function persistShows(showObjects) {
-    console.log("Attempting to persist into Mongo collection: Tvlistings");
+    console.log("Attempting to persist into TVListings collection: Tvlistings");
 
-    //for (var showobj in showObjects) {
-        Tvlistings.insert(showObjects[i], function (e) {
-            if (e) errorAction(e.message); else submitAction();
+    for(var x=0; x < showObjects.length; x++) {
+        Tvlistings.insert(showObjects[x], function (e) {
+            if (e) errorAction(e.message);
         });
-    //}
+    }
 }
 
-function Show(showname, showid) {
-    this.name = showname;
+function Show(showname, showid, chanNum, chanName) {
     this.id = showid;
+    this.name = showname;
+    this.channelname = chanName;
+    this.channelnumber = chanNum;
+    this.cableprovider = "Verizon FiOS";
 
     this.printDetails = function() {
-        console.log("Name: " + this.name + "\nID: " + this.id + "\n");
+        console.log("Name: " + this.name + "\nID: " + this.id +
+            "\nChannel Name: " + this.channelname + "\nChannel Number: " + this.channelnumber +"\n");
     }
 }
 
